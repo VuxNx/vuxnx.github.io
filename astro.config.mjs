829b98194cd1baeffdd29cb57d65b7e88e517854
VuxNx <nghia.vutrong@hcmut.edu.vn>
2025-08-24 11:18:07 +0700

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://vuxnx.github.io",
+	site: "https://vuxnx.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
